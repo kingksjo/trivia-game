fd5c1b60b8d824d0e228a389a180eab34d0a50a3
kingksjo <kamiye06@gmail.com>
2025-01-03 14:55:33 +0100

    Added functionality to UI

--- ui.py
+++ ui.py
@@ -1,10 +1,12 @@
 import tkinter as tk
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#87CEEB"
 
 class QuizUI:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = tk.Tk()
         self.window.title("Quizzer")
         self.window.minsize(width=350, height=560)
@@ -16,18 +18,36 @@ class QuizUI:
         self.score_label.grid(row=0, column=2)
 
         self.canvas = tk.Canvas(width=300, height=250, background="white", highlightthickness=0)
-        self.question_text = self.canvas.create_text(150, 125, text="Question Goes Here", font=("Arial", 16, "italic"))
+        self.question_text = self.canvas.create_text(150, 125, text="Question Goes Here",
+                                                     width=280,
+                                                     font=("Arial", 16, "italic")
+                                                     )
         self.canvas.grid(row=1, column=1, columnspan=2, pady=20)
 
         correct_image = tk.PhotoImage(file=r"images//true.png")
-        self.correct_button = tk.Button(image=correct_image, highlightthickness=0)
+        self.correct_button = tk.Button(image=correct_image, highlightthickness=0, command=self.user_says_true)
         self.correct_button.grid(row=2, column=1)
 
         wrong_image = tk.PhotoImage(file=r"images//false.png")
-        self.wrong_button = tk.Button(image=wrong_image, highlightthickness=0)
+        self.wrong_button = tk.Button(image=wrong_image, highlightthickness=0, command=self.user_says_false)
         self.wrong_button.grid(row=2, column=2)
 
-
+        self.get_next_question()
 
 
         self.window.mainloop()
+
+    def get_next_question(self):
+        ques_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=ques_text)
+
+    def user_says_true(self):
+        self.quiz.check_answer("True")
+        self.get_next_question()
+
+    def user_says_false(self):
+        self.quiz.check_answer("False")
+        self.get_next_question()
+
+
+
